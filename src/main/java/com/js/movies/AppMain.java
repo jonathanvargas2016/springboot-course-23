2d915e03de9bd754513fd7d08faa8a97a6d15e59
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2022-11-02 20:38:39 -0500

    add web dependency

--- src/main/java/com/js/movies/AppMain.java
+++ src/main/java/com/js/movies/AppMain.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppMain {
 
 	public static void main(String[] args) {
-
+		System.out.println("Project is working....");
 		SpringApplication.run(AppMain.class, args);
 	}
 
