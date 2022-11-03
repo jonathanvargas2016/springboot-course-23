7da5739a10d670ce7bacb45da9b850409eb6f50c
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2022-11-02 20:34:43 -0500

    project init

---
+++ src/test/java/com/js/movies/SpringMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
