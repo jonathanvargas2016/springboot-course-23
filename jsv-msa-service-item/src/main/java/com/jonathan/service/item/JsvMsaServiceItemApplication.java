33f76ecb635aaf8e80c6760401cf8cfd3df63262
Jonathan Vargas <jonathan.1996mds@gmail.com>
2023-08-03 20:43:26 -0500

    fix: change to load balancer

--- jsv-msa-service-item/src/main/java/com/jonathan/service/item/JsvMsaServiceItemApplication.java
+++ jsv-msa-service-item/src/main/java/com/jonathan/service/item/JsvMsaServiceItemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients //habilitar clientes feign ademas permite inyectar estos clientes en los controllers u otros clientes spring
 @SpringBootApplication
-@LoadBalancerClient(value = "services-products")
+//@LoadBalancerClient(value = "services-products")
 public class JsvMsaServiceItemApplication {
 
 	public static void main(String[] args) {
