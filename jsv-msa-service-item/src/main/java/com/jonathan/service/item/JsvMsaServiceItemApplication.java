0e15cd65fc5eb905ee94265005af5e8a5f1d1194
Jonathan Vargas <jonathan.1996mds@gmail.com>
2023-08-09 23:55:02 -0500

    feat: implementation of eureka server

--- jsv-msa-service-item/src/main/java/com/jonathan/service/item/JsvMsaServiceItemApplication.java
+++ jsv-msa-service-item/src/main/java/com/jonathan/service/item/JsvMsaServiceItemApplication.java
@@ -3,6 +3,7 @@ package com.jonathan.service.item;
 import com.jonathan.service.item.configuration.InstanceSupplierConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
