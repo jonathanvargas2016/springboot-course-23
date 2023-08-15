247371126505e09b03dd788857b0f0e48e733b66
Jonathan Vargas <jonathan.1996mds@gmail.com>
2023-08-14 22:08:35 -0500

    feat: implementation of eureka

--- jsv-msa-service-item/src/main/java/com/jonathan/service/item/JsvMsaServiceItemApplication.java
+++ jsv-msa-service-item/src/main/java/com/jonathan/service/item/JsvMsaServiceItemApplication.java
@@ -1,16 +1,12 @@
 package com.jonathan.service.item;
 
-import com.jonathan.service.item.configuration.InstanceSupplierConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 //habilitar clientes feign ademas permite inyectar estos clientes en los controllers u otros clientes spring
 @SpringBootApplication
-@LoadBalancerClient(name = "services-products", configuration = InstanceSupplierConfiguration.class)
 public class JsvMsaServiceItemApplication {
 
     public static void main(String[] args) {
