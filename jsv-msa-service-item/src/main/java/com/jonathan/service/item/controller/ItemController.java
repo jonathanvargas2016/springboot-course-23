54e4c0716fd052879607f99fe9c2f5de6a389330
Jonathan Vargas <jonathan.1996mds@gmail.com>
2023-08-28 23:08:11 -0500

    feat: controlar el error timeout

--- jsv-msa-service-item/src/main/java/com/jonathan/service/item/controller/ItemController.java
+++ jsv-msa-service-item/src/main/java/com/jonathan/service/item/controller/ItemController.java
@@ -5,6 +5,7 @@ import com.jonathan.service.item.domain.Product;
 import com.jonathan.service.item.service.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @AllArgsConstructor
@@ -28,13 +30,45 @@ public class ItemController {
         return new ResponseEntity<>(itemService.findAll(), HttpStatus.OK);
     }
 
+//    @GetMapping("/{id}/amount/{amount}")
+//    @Retry(name = "anotherMethod", fallbackMethod = "anotherMethod")
+//    public ResponseEntity<Item> detail(@PathVariable Long id, @PathVariable Integer amount) {
+//        return new ResponseEntity<>(itemService.findById(id, amount), HttpStatus.OK);
+//    }
+
+
+//    public ResponseEntity<Item> anotherMethod(@PathVariable Long id, @PathVariable Integer amount, Throwable throwable) {
+//        System.out.println("ENTRO METODO ALTERNATIVO ---------------------->");
+//        Item item = new Item();
+//        Product product = new Product();
+//        product.setId(id);
+//        product.setName("Camara Sony");
+//        product.setPrice(new BigDecimal(1500));
+//        item.setAmount(amount);
+//        item.setProduct(product);
+//
+//        return new ResponseEntity<>(item, HttpStatus.OK)
+//
+//    }
+
     @GetMapping("/{id}/amount/{amount}")
+    @TimeLimiter(name = "detail")
     @Retry(name = "anotherMethod", fallbackMethod = "anotherMethod")
-    public ResponseEntity<Item> detail(@PathVariable Long id, @PathVariable Integer amount) {
-        return new ResponseEntity<>(itemService.findById(id, amount), HttpStatus.OK);
+    public CompletableFuture<ResponseEntity<Item>> detail(@PathVariable Long id, @PathVariable Integer amount) {
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return new ResponseEntity<>(itemService.findById(id, amount), HttpStatus.OK);
+
+        });
+
     }
 
-    public ResponseEntity<Item> anotherMethod(@PathVariable Long id, @PathVariable Integer amount, Throwable throwable) {
+    public CompletableFuture<ResponseEntity<Item>> anotherMethod(@PathVariable Long id, @PathVariable Integer amount, Throwable throwable) {
         System.out.println("ENTRO METODO ALTERNATIVO ---------------------->");
         Item item = new Item();
         Product product = new Product();
@@ -43,7 +77,10 @@ public class ItemController {
         product.setPrice(new BigDecimal(1500));
         item.setAmount(amount);
         item.setProduct(product);
-        return new ResponseEntity<>(item, HttpStatus.OK);
+
+        return CompletableFuture.completedFuture(
+                new ResponseEntity<>(item, HttpStatus.OK)
+        );
     }
 
 
