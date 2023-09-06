c332071d3ba6a676ad5e30e5e5394f6ece6ac83e
Jonathan Vargas <jonathan.1996mds@gmail.com>
2023-09-05 23:38:08 -0500

    feat: rules spring cloud gateway

---
+++ jsv-msa-zuul-server/src/main/java/com/jonathan/zuul/filters/ExampleGlobalFilters.java
@@ -0,0 +1,44 @@
+package com.jonathan.zuul.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+@Component
+public class ExampleGlobalFilters implements GlobalFilter, Ordered {
+
+    private final Logger logger = LoggerFactory.getLogger(ExampleGlobalFilters.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Pre Filter executed");
+
+        exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
+
+        // mono, permite crear un objeto reactivo que hara algo.
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Executing post filter");
+
+            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token"))
+                    .ifPresent(value -> exchange.getResponse().getHeaders().add("token", value));
+
+            exchange.getResponse().getCookies()
+                    .add("Colour", ResponseCookie.from("colour", "red").build());
+//            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
