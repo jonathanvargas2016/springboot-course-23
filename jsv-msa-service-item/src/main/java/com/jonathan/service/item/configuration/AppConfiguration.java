01d30db2cb9c261074ce8aeab37ab31e989a3fb7
jvargasn <jvargasn@pichincha.com>
2023-09-18 23:04:32 -0500

    feat: configuration circuitbreaker

--- jsv-msa-service-item/src/main/java/com/jonathan/service/item/configuration/AppConfiguration.java
+++ jsv-msa-service-item/src/main/java/com/jonathan/service/item/configuration/AppConfiguration.java
@@ -1,8 +1,15 @@
 package com.jonathan.service.item.configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfiguration {
@@ -12,5 +19,24 @@ public class AppConfiguration {
         return new RestTemplate();
     }
 
+    @Bean()
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> {
+            return new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(
+                            CircuitBreakerConfig.custom()
+                                    .slidingWindowSize(10) //permite 10 peticiones.
+                                    .failureRateThreshold(50) //fallas mayor al 50%
+                                    .waitDurationInOpenState(Duration.ofSeconds(10L))
+                                    .permittedNumberOfCallsInHalfOpenState(5)
+                                    .slowCallRateThreshold(50) //porcentaje de llamadas lentas. Cuando el porcentaje es mayor que el umbral CircuitBreaker pasa a estado abierto.
+                                    .slowCallDurationThreshold(Duration.ofSeconds(2L))
+                                    .build()
+                    ).timeLimiterConfig(TimeLimiterConfig
+                            .custom()
+                            .timeoutDuration(Duration.ofSeconds(3L)).build())
+                    .build();
+        });
+    }
 
 }
